b6a0be795ee72ad3e853b6e0b5fc81173285e34b
Saurabh Vaidya <saurabhvaidya2003@gmail.com>
2025-09-15 15:22:59 +0530

    First commit - Recipe Project

---
+++ main/urls.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.urls import path
+from vege.views import receipes
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from vege.views import receipes, delete_receipe, update_receipe, login_page ,register_page, logout_page
+from vege import views
+
+
+urlpatterns = [
+    path('', receipes, name="home"),           # root URL → shows recipes
+    path('receipes/', receipes, name="receipes"),
+    path('delete-receipe/<id>/',delete_receipe, name="delete_receipe"),
+    path('update-receipe/<id>/',update_receipe, name="update_receipe"),
+    path('admin/', admin.site.urls),
+    path('login/', login_page , name='login_page'),
+    path('register/', register_page , name='register_page'),
+    path('logout/', logout_page , name="logout_page"),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
